5d858908ba333ec9ad194a589d54819aecd0c925
mrchidubem8 <mrchidubem9@gmail.com>
2023-03-20 12:13:28 -0700

    Function prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,22 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- *  * struct dog - a dog's basic info
- *   * @name: Name of the dog
- *    * @age: Age of the dog
- *     * @owner: Who is the owner of the dog
- *      *
- *       * Description: No description for this struct
- *        */
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+char *name;
+float age;
+char *owner;
 };
-
-/**
- *  * dog_t - typedef for struct dog
- *   */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
